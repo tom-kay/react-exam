584c2218faa824c40b258d93f5ae4ddb8ac752c2
tom-kay <74287458+tom-kay@users.noreply.github.com>
2023-07-17 01:40:18 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+/* eslint-env node */
+
+module.exports = {
+  env: { browser: true, es2021: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-ally/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react', 'react-hooks', 'jsx-ally'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single', { allowtemplateLiterals: true }],
+  },
+};
